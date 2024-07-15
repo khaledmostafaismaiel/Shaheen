d47ddc71ce36c36aaceac19893edf78cf988fdc5
khaled Mostafa <khaledmostafa297@gmail.com>
2024-07-15 16:01:22 +0300

    BIR-11: install laravel/ui

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::middleware(['auth'])->group(function () {
+    Route::get('/', function () {
+        return view('welcome');
+    });
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
